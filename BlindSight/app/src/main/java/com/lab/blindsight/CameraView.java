cd96a8fc94b67acf1c60aeb1faeb7c11c41e2ea0
Louis Vichy <lgvichy@gmail.com>
2016-11-12 02:37:29 -0500

    Camera done

---
+++ BlindSight/app/src/main/java/com/lab/blindsight/CameraView.java
@@ -0,0 +1,71 @@
+package com.lab.blindsight;
+
+import android.content.Context;
+
+import android.hardware.Camera;
+
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by lab on 11/12/16.
+ */
+
+public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder holder;
+
+    private Camera camera;
+
+    public CameraView(Context context, Camera camera) {
+        super(context);
+
+        this.camera = camera;
+        this.camera.setDisplayOrientation(90);
+        //get the holder and set this class as the callback, so we can get camera data here
+        this.holder = getHolder();
+        this.holder.addCallback(this);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        //when the surface is created, we can set the camera to draw images in this surfaceholder
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            Log.d("ERROR", "Camera error on surfaceCreated " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+        if (this.holder.getSurface() == null)//check if the surface is ready to receive camera data
+            return;
+        try {
+            this.camera.stopPreview();
+        } catch (Exception e) {
+            //this will happen when you are trying the camera if it's not running
+
+        }
+
+        //now, recreate the camera preview
+        try {
+            this.camera.setPreviewDisplay(this.holder);
+            this.camera.startPreview();
+        } catch (IOException e) {
+            Log.d("ERROR", "Camera error on surfaceChanged " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        this.camera.stopPreview();
+        this.camera.release();
+    }
+
+}
--- BlindSight/app/src/main/java/com/lab/blindsight/Stream.java
+++ BlindSight/app/src/main/java/com/lab/blindsight/Stream.java
@@ -1,10 +1,18 @@
 package com.lab.blindsight;
 
+import android.hardware.Camera;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 public class Stream extends AppCompatActivity {
+
+    private Camera camera;
+    private CameraView cameraView = null;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -17,8 +25,27 @@ public class Stream extends AppCompatActivity {
         setContentView(R.layout.activity_stream);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
+        try {
+            this.camera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e) {
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if (this.camera != null) {
+            this.cameraView = new CameraView(this, this.camera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout) findViewById(R.id.camera_view);
+            camera_view.addView(this.cameraView);//add the SurfaceView to the layout
+        }
+        //btn to close the application
+        ImageButton imgClose = (ImageButton) findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
     }
 
     /**
